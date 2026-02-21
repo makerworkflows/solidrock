a771bf85264ff76b9ef1d95c6e413f1efdee2a1c
Guillermo Aristi <g.zamorano.aristi@icloud.com>
2026-02-21 11:34:38 -0600

    build: configured static export and github actions for git pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+  basePath: '/solidrock',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
